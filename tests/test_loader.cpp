fb97432d3cb4c4832e06fe62b52856fbb0f3fa77
ManifoldFR <wilson.jallet@polytechnique.org>
2024-11-21 16:11:06 +0100

    Link to fmt, add test_loader.cpp

---
+++ tests/test_loader.cpp
@@ -0,0 +1,48 @@
+#include "robot_descriptions_cpp/robot_load.hpp"
+#include "robot_descriptions_cpp/robot_spec.hpp"
+#include <gtest/gtest.h>
+
+using namespace robot_descriptions;
+
+GTEST_TEST(LoadSpec, UR5) {
+  robot_spec spec = loadRobotSpecFromToml("ur.toml", "ur5", true);
+  EXPECT_TRUE(fs::exists(spec.urdfPath));
+}
+
+GTEST_TEST(LoadSpec, ANYmalB) {
+  robot_spec spec = loadRobotSpecFromToml("ANYmal.toml", "anymal", true);
+  EXPECT_TRUE(fs::exists(spec.urdfPath));
+}
+
+GTEST_TEST(LoadSpec, ANYmalC) {
+  robot_spec spec = loadRobotSpecFromToml("ANYmal.toml", "anymal_c", true);
+  EXPECT_TRUE(fs::exists(spec.urdfPath));
+}
+
+GTEST_TEST(LoadTest, UR5) {
+  pinocchio::Model model;
+  robot_descriptions::loadModelFromToml("ur.toml", "ur5", model);
+  EXPECT_EQ(model.njoints, 7);
+  EXPECT_EQ(model.nq, 6);
+  EXPECT_EQ(model.name, "ur5");
+}
+
+GTEST_TEST(LoadTest, DoublePendulum) {
+  pinocchio::Model model;
+  robot_descriptions::loadModelFromToml("double_pendulum.toml", "double_pendulum",
+                                    model);
+  EXPECT_EQ(model.njoints, 3);
+  EXPECT_EQ(model.nq, 2);
+  EXPECT_EQ(model.nv, 2);
+  EXPECT_EQ(model.name, "2dof_planar");
+}
+
+GTEST_TEST(LoadTest, DoublePendulumContinuous) {
+  pinocchio::Model model;
+  robot_descriptions::loadModelFromToml("double_pendulum.toml",
+                                    "double_pendulum_continuous", model);
+  EXPECT_EQ(model.njoints, 3);
+  EXPECT_EQ(model.nq, 4);
+  EXPECT_EQ(model.nv, 2);
+  EXPECT_EQ(model.name, "2dof_planar");
+}
