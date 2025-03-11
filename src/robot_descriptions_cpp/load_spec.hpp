27fea894c3538b672fbde2b35120c611995feb6b
ManifoldFR <wilson.jallet@polytechnique.org>
2025-03-11 14:24:37 +0100

    Change defines, add install interface definition for TOML files, install them

--- src/robot_descriptions_cpp/load_spec.hpp
+++ src/robot_descriptions_cpp/load_spec.hpp
@@ -10,7 +10,8 @@ namespace fs = std::filesystem;
 inline static const fs::path EXAMPLE_ROBOT_DATA_PACKAGE_DIRS =
     fs::path(EXAMPLE_ROBOT_DATA_MODEL_DIR) / "../..";
 
-inline static constexpr std::string_view DEFAULT_TOML_DIR = ROBOT_TOML_DIR;
+inline static constexpr std::string_view DEFAULT_TOML_DIR =
+    ROBOT_DESCRIPTIONS_ASSET_DIR;
 
 robot_spec loadErdRobotSpecFromToml(std::string_view filename,
                                     std::string_view key, bool verbose = false);
